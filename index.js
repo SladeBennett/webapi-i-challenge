43aedc6545534e1c5cd694060e233b34f0800a29
SladeBennett <slade8colt@gmail.com>
2024-08-05 22:41:43 -0600

    NearComplete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// implement your API here
+const express = require('express')
+const methods = require('./data/db.js')
+
+const server = express()
+
+server.get('/users', (req, res) => {
+   res.status(200).json('hello')
+   methods.find()
+})
+
+server.listen(5000, () => console.log('Server Api running on 5000'))
